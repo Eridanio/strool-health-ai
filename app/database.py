4362df69707b6d32bfda1ce349b08d13afc70bf3
Eridanio <adilsoneridanio@gmail.com>
2026-03-29 07:06:30 +0100

    add database integration and auto table creation

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = "postgresql://strool_user:SUA_SENHA@dpg-d747th1aae7s73bcdvkg-a.oregon-postgres.render.com/strool"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
